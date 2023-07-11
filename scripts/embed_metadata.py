bca315043a45703200c19c826982b37e88dd73df
neosheo <neosheo@proton.me>
2023-07-11 18:41:01 -0500

    fixed quotation consistency on embed_metadata.py and added line to remove album tags if present

--- scripts/embed_metadata.py
+++ scripts/embed_metadata.py
@@ -5,8 +5,9 @@ file = OggOpus(sys.argv[1])
 
 file['title'] = sys.argv[1].split('/')[3]
 file['artist'] = sys.argv[2]
-file["config"] = sys.argv[3]
-file["station"] = 'new'
+file['config'] = sys.argv[3]
+file['station'] = 'new'
+file['album'] = ''
 file.save()
 
 
