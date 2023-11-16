b7e9e2bb8c45d60ec8f08aec039658c84c565aab
neosheo <neosheo@proton.me>
2023-11-15 20:42:36 -0600

    added delay to skip feature so users don't need to manually reset stream

--- webapp/static/skip_song.js
+++ webapp/static/skip_song.js
@@ -1,7 +1,12 @@
-function skipSong() {
+async function skipSong() {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const audioElement = document.getElementById("audio-controls");
     const url = window.location.href.split("/");
 	const station = url[url.length - 1];
     fetch(`/skip/${station}`);
+    await delay(5000);
+    audioElement.pause();
+    audioElement.play();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
