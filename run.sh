78c47b1c2ffca55d4c3b8b39d5d0b80051a740ef
neosheo <neosheo@proton.me>
2023-12-11 09:51:55 -0600

    removed sed command that caused script to run forever, deleted yt-dlp and pip upgrade in run script

--- run.sh
+++ run.sh
@@ -6,9 +6,6 @@ selection_log=logs/cogmera_selection.log
 echo "$(date)" > "$download_log"
 echo "$(date)" > "$selection_log"
 
-#python -m pip install -U pip | tee --append "$download_log" 2>&1 \
-#	&& python -m pip install -U yt-dlp --user | tee --append "$download_log" 2>&1
-
 python cogmera.py | tee --append "$selection_log" 2>&1
 
 log=/stringwave/logs/pipefeeder.log
@@ -16,6 +13,3 @@ log=/stringwave/logs/pipefeeder.log
 echo "$(date)" > "$log"
 
 python pipefeeder.py | tee --append "$log" 2>&1
-sed '/^\/stringwave\//d' logs/pipefeeder.log
-	
-echo >> "$log"
